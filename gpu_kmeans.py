1753fd40642a6a7f2969962ba8eb321533c7ff48
RayFan123 <48203984+RayFan123@users.noreply.github.com>
2021-12-15 18:05:47 -0500

    Update gpu_kmeans.py

--- gpu_kmeans.py
+++ gpu_kmeans.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 import numpy as np
 import pycuda.driver as cuda
 import pycuda.gpuarray as gpuarray
@@ -18,14 +19,47 @@ class GPU:
         kernelwrapper = """#include <stdio.h>
             __global__ void dist(float* A, const float* C, int* Y, int n, int m, int k){
                 int tid = blockIdx.x * blockDim.x + threadIdx.x;
+                int t = threadIdx.x;
                 if (tid < n){
-                    float d = 999999.0f;
+                    float d = 999999.9f;
                     int index = 0;
                     float current = 0.0f;
                     for (int j = 0; j < k; j ++){
                         current = 0;
                         for (int i = 0; i < m; i ++){
                             current += pow((A[tid * m + i] - C[j * m + i]), 2);
+                            //current += pow((data[t][i] - C[j * m + i]), 2);
+                        }
+                        //printf("  dist:%f;d:%f  ", current,d);
+                        if(current < d){
+                            //printf("change");
+                            d = current;
+                            index = j;
+                        }
+                    }
+                    Y[tid] = index;
+                    //printf(" index:%d ", index);
+                }
+            }      
+            __global__ void dist_shared(float* A, const float* C, int* Y, int n, int m, int k){
+                int tid = blockIdx.x * blockDim.x + threadIdx.x;
+                int t = threadIdx.x;
+                __shared__ float data[256][8];
+                if (tid < n){
+                    for (int i = 0; i< m; i ++){
+                        data[t][i] = A[tid * m + i];
+                    }
+                }
+                __syncthreads();
+                if (tid < n){
+                    float d = 999999.9f;
+                    int index = 0;
+                    float current = 0.0f;
+                    for (int j = 0; j < k; j ++){
+                        current = 0;
+                        for (int i = 0; i < m; i ++){
+                            //current += pow((A[tid * m + i] - C[j * m + i]), 2);
+                            current += pow((data[t][i] - C[j * m + i]), 2);
                         }
                         //printf("  dist:%f;d:%f  ", current,d);
                         if(current < d){
@@ -96,21 +130,61 @@ class GPU:
                     //C[k*m + tid] = data[0][tid];
                 }
             }
-                
+            
+            __global__ void sum_partial(float* A, float* diff, int* Y_1, int* Y_2, int n, int m, int k, int* d_count){
+                int tid = blockIdx.x * blockDim.x + threadIdx.x;
+                if (tid < n){
+                    int old = Y_1[tid];
+                    int cur = Y_2[tid];
+                    if (old >= k){
+                        //printf(" cur:%d ", cur);
+                        //printf(" count:%d ", d_count[cur]);
+                        for (int i = 0;i < m;i++){
+                            atomicAdd( & (diff[cur * m + i]), A[tid * m + i]);
+                            atomicAdd( & (d_count[cur]), 1);
+                        }
+                    }
+                    else{
+                        if (old != cur){
+                            for (int i = 0;i < m;i++){
+                                atomicAdd( & (diff[old * m + i]), -A[tid * m + i]);
+                                atomicAdd( & (diff[cur * m + i]), A[tid * m + i]);
+                                atomicAdd( & (d_count[old]), -1);
+                                atomicAdd( & (d_count[cur]), 1);
+                            }
+                        }
+                    }
+                }
+            }
+            
+            __global__ void mean_partial(float* C, float* diff, int m, int k, int* count, int* d_count){
+                int tid = threadIdx.x;
+                if (tid < k){
+                    for (int i = 0; i<m;i++){
+                        C[tid * m + i] = (C[tid * m + i] * count[tid] + diff[tid * m + i]) / (count[tid] + d_count[tid]);
+                    }
+                    count[tid] += d_count[tid];
+                }
+            }
+            
+            __global__ void update(int* Y_1, int* Y_2,int n){
+                int tid = blockIdx.x * blockDim.x + threadIdx.x;
+                if (tid < n){
+                    Y_1[tid] = Y_2[tid];
+                }
+            }
         """
         return SourceModule(kernelwrapper)
 
-
-# 计算欧几里得距离
 def distEclud(vecA, vecB):
-    return np.sqrt(np.sum(np.power(vecA - vecB, 2)))  # 求两个向量之间的距离
+    return np.sqrt(np.sum(np.power(vecA - vecB, 2)))
 
-# 构建聚簇中心，取k个(此例中为4)随机质心
+
 def randCent(dataSet, k):
     m = np.shape(dataSet)[1]
     n = np.shape(dataSet)[0]
     index = np.random.randint(0,n,k)
-    centroids = np.zeros((k,m))  # 每个质心有n个坐标值，总共要k个质心
+    centroids = np.zeros((k,m))
     for j in range(k):
         centroids[j, :] = dataSet[index[j]]
     return centroids
@@ -119,7 +193,7 @@ def randCent(dataSet, k):
 def cluster_assign_python(dataSet, centroids, clusterAssment, distMeans,clusterChanged):
     m = np.shape(dataSet)[0]
     k = np.shape(centroids)[0]
-    for i in range(m):  # 把每一个数据点划分到离它最近的中心点
+    for i in range(m):
         minDist = np.inf
         minIndex = -1
         for j in range(k):
@@ -127,11 +201,11 @@ def cluster_assign_python(dataSet, centroids, clusterAssment, distMeans,clusterC
             #print(distJI)
             if distJI < minDist:
                 minDist = distJI
-                minIndex = j  # 如果第i个数据点到第j个中心点更近，则将i归属为j
+                minIndex = j
         if (clusterAssment[i] != minIndex):
-            clusterChanged = True  # 如果分配发生变化，则需要继续迭代
+            clusterChanged = True
             clusterAssment[i] = minIndex
-              # 并将第i个数据点的分配情况存入字典
+
     return clusterAssment,clusterChanged
 
 def cluster_assign_CUDA(dataSet, centroids, clusterAssment, distMeans,clusterChanged):
@@ -177,31 +251,34 @@ def Mean_cal_CUDA(dataSet,clusterAssment,centroids,k):
     cuda.memcpy_dtoh(count, count_gpu)
     #print(sum(count))
     return centroids
-# k-means 聚类算法
+
 def kMeans(dataSet, k, gpu, means, init_centroids,distMeans=distEclud):
     start = time.time()
     m = np.shape(dataSet)[0]
     centroids = init_centroids.copy()
-    clusterAssment = np.zeros(m).astype(np.int32)  # 用于存放该样本属于哪类及质心距离
-    # clusterAssment第一列存放该数据所属的中心点，第二列是该数据到中心点的距离
-    clusterChanged = True  # 用来判断聚类是否已经收敛
+    clusterAssment = np.zeros(m).astype(np.int32)
+
+    clusterChanged = True
     iter = 0
     while clusterChanged:
         clusterChanged = False
         if(gpu == True):
+            temp = np.copy(clusterAssment)
             clusterAssment,clusterChanged = cluster_assign_CUDA(dataSet, centroids, clusterAssment, distMeans, clusterChanged)
+            chan = np.count_nonzero(temp-clusterAssment)
+            # print(chan)
         else:
             clusterAssment, clusterChanged = cluster_assign_python(dataSet, centroids, clusterAssment, distMeans,clusterChanged)
         #print("clusterAssment",clusterAssment)
         if means == True:
             centroids = Mean_cal_CUDA(dataSet, clusterAssment, centroids, k)
         else:
-            for cent in range(k):  # 重新计算中心点
-                ptsInClust = dataSet[np.nonzero((clusterAssment == cent).astype(int))]  # 去第一列等于cent的所有列
-                centroids[cent, :] = np.mean(ptsInClust, axis=0)  # 算出这些数据的中心点
+            for cent in range(k):
+                ptsInClust = dataSet[np.nonzero((clusterAssment == cent).astype(int))]
+                centroids[cent, :] = np.mean(ptsInClust, axis=0)
         iter += 1
-    print(iter)
     end = time.time()
+    print(iter)
     return centroids, clusterAssment,end - start,iter
 
 def Kmeans_gpu_integrate(dataSet, k, init_centroids, use_share):
@@ -222,13 +299,19 @@ def Kmeans_gpu_integrate(dataSet, k, init_centroids, use_share):
     count_gpu = cuda.mem_alloc(count.nbytes)
     cuda.memcpy_htod(A_gpu, dataSet)
     cuda.memcpy_htod(C_gpu, centroids)
+    time_ = 0
     iter = 0
     while clusterChanged:
         clusterChanged = False
         temp = clusterAssment.copy()
-
+        cstart = cuda.Event()
+        end = cuda.Event()
+        cstart.record()
         func1 = mod.get_function("dist")
         func1(A_gpu, C_gpu, Y_gpu, np.int32(n), np.int32(m), np.int32(k), block=blockDim, grid=gridDim)
+        end.record()
+        end.synchronize()
+        time_ = time_ + cstart.time_till(end) * 1e-3
 
         func2 = mod.get_function("Mean_initial")
         func2(C_gpu, np.int32(m), np.int32(k), block=(256,1,1), grid=(1,1,1))
@@ -249,7 +332,7 @@ def Kmeans_gpu_integrate(dataSet, k, init_centroids, use_share):
         # cuda.memcpy_dtoh(count, count_gpu)
         # print(count)
         cuda.memcpy_dtoh(clusterAssment, Y_gpu)
-        cuda.memcpy_dtoh(centroids, C_gpu)
+        # cuda.memcpy_dtoh(centroids, C_gpu)
         # print(centroids)
         if ((temp == clusterAssment).all()):
             pass
@@ -259,31 +342,114 @@ def Kmeans_gpu_integrate(dataSet, k, init_centroids, use_share):
     cuda.memcpy_dtoh(centroids, C_gpu)
     print(iter)
     end = time.time()
-    return centroids, clusterAssment, end - start, iter
-# --------------------测试----------------------------------------------------
-# 用测试数据及测试kmeans算法
+    return centroids, clusterAssment, end - start, iter,time_
+
+def Kmeans_gpu_partial(dataSet, k, init_centroids):
+    start = time.time()
+    n = np.shape(dataSet)[0]
+    m = np.shape(dataSet)[1]
+    centroids = init_centroids.copy()
+    clusterAssment = np.zeros(n).astype(np.int32)
+    diff = np.zeros((k,m)).astype(np.float32)
+    clusterChanged = True
+    s_len = int(math.ceil(n / 256))
+    blockDim = (256, 1, 1)
+    gridDim = (s_len, 1, 1)
+    A_gpu = cuda.mem_alloc(dataSet.nbytes)
+    C_gpu = cuda.mem_alloc(centroids.nbytes)
+    Diff_gpu = cuda.mem_alloc(centroids.nbytes)
+    Y_gpu = cuda.mem_alloc(clusterAssment.nbytes)
+    Y1_gpu = cuda.mem_alloc(clusterAssment.nbytes)
+
+    count = np.zeros(k).astype(np.int32)
+    z = np.zeros(k).astype(np.int32)
+    count_gpu = cuda.mem_alloc(count.nbytes)
+    D_count_gpu = cuda.mem_alloc(count.nbytes)
+    cuda.memcpy_htod(Y1_gpu, (np.ones(n)+k).astype(np.int32))
+    cuda.memcpy_htod(A_gpu, dataSet)
+    cuda.memcpy_htod(C_gpu, centroids)
+    cuda.memcpy_htod(count_gpu, z)
+    iter = 0
+    time_ = 0
+    while clusterChanged:
+        cstart = cuda.Event()
+        end = cuda.Event()
+        cstart.record()
+        clusterChanged = False
+        temp = clusterAssment.copy()
+
+        func1 = mod.get_function("dist")
+        func1(A_gpu, C_gpu, Y_gpu, np.int32(n), np.int32(m), np.int32(k), block=blockDim, grid=gridDim)
+        # end.record()
+        # end.synchronize()
+        #time_ = time_ + cstart.time_till(end) * 1e-3
+        cuda.memcpy_htod(D_count_gpu, z)
+        cuda.memcpy_htod(Diff_gpu, diff)
+        func3 = mod.get_function("sum_partial")
+        func3(A_gpu, Diff_gpu, Y1_gpu, Y_gpu, np.int32(n), np.int32(m),np.int32(k),  D_count_gpu, block=(512, 1, 1),grid=(int(math.ceil(n / 512)), 1, 1))
+
+
+        func4 = mod.get_function("mean_partial")
+        func4(C_gpu, Diff_gpu, np.int32(m), np.int32(k), count_gpu, D_count_gpu, block=(32,1,1), grid=(1,1,1))
+
+
+        func2 = mod.get_function("update")
+        func2(Y1_gpu, Y_gpu, np.int32(n), block=blockDim, grid=gridDim)
+        # cuda.memcpy_dtoh(count, count_gpu)
+        # print(count)
+        cuda.memcpy_dtoh(clusterAssment, Y_gpu)
+        end.record()
+        end.synchronize()
+        time_ = time_ + cstart.time_till(end) * 1e-3
+        #cuda.memcpy_dtoh(centroids, C_gpu)
+        # print(centroids)
+        if ((temp == clusterAssment).all()):
+            pass
+        else:
+            clusterChanged = True
+        iter += 1
+    cuda.memcpy_dtoh(centroids, C_gpu)
+    print(iter)
+    end = time.time()
+    return centroids, clusterAssment, end - start, iter,time_
+
+
+
 m = 8
-k = 8
-x = np.random.normal(loc=1, scale=2, size=(10000, m))
-y = np.random.normal(loc=3, scale=1, size=(10000, m))
-dataset = np.vstack((x,y)).astype(np.float32)
-init_centroids = randCent(dataset, k).astype(np.float32)
+k = 5
+n = [100,1000,10000,100000,1000000]
+#y = np.random.normal(loc=3, scale=1, size=(50000, m))
+
+
+t = [0,0,0,0,0]
+iter = [0,0,0,0,0]
 inst = GPU()
 mod = inst.mod
+for j in range(5):
+    x = np.random.normal(loc=1, scale=2, size=(n[j], m))
+    dataset = x.astype(np.float32)
+    for i in range(10):
+        init_centroids = randCent(dataset, k).astype(np.float32)
+        myCentroids, clustAssign,t_t,iter_t = kMeans(dataset, k, True, False, init_centroids)
+        t[j] += t_t
+        iter[j] += iter_t
+    t[j] /= 10
+    iter[j] /=10
 
-#myCentroids, clustAssign,t1,iter1 = kMeans(dataset, k, True, True, init_centroids)
-myCentroids, clustAssign,t,iter = Kmeans_gpu_integrate(dataset, k, init_centroids,True)
-myCentroids2, clustAssign2,t2,iter2 = Kmeans_gpu_integrate(dataset, k, init_centroids,False)
-
-#myCentroids, clustAssign,t3,iter3 = kMeans(dataset, k, True, False, init_centroids)
+# myCentroids, clustAssign,t,iter,time_ = Kmeans_gpu_partial(dataset, k, init_centroids)
+#myCentroids2, clustAssign2,t2,iter2,time_1 = Kmeans_gpu_integrate(dataset, k, init_centroids,False)
+#myCentroids2, clustAssign2,t2,iter2,time_1 = Kmeans_gpu_integrate(dataset, k, init_centroids,True)
+#myCentroids3, clustAssign3,t3,iter3 = kMeans(dataset, k, True, False, init_centroids)
 #myCentroids, clustAssign,t2,iter2 = kMeans(dataset, 3, False,init_centroids)
 
-start = time.time()
-cluster = KMeans(n_clusters=k,max_iter=2000,n_init = 1,init = init_centroids,algorithm="full",tol=0.00000001).fit(dataset)
-end = time.time()
-print(cluster.n_iter_)
-print(t,t2,end-start)
-print(myCentroids)
-print(cluster.cluster_centers_)
-print(clustAssign)
+# start = time.time()
+# cluster = KMeans(n_clusters=k,max_iter=2000,n_init = 1,init = init_centroids,algorithm="full",tol=0.00000001).fit(dataset)
+# end = time.time()
 
+# print(cluster.n_iter_)
+# print(time_,t,time_1,t2,end-start)
+# print(myCentroids)
+# print(cluster.cluster_centers_)
+
+print(t)
+print(iter)
